222170fd4eeafe08de75d92a970f90900fb4956e
Abimbola Okusada <44692035+abimbola001@users.noreply.github.com>
2024-03-08 08:37:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+terraform {
+     backend "remote" {
+    # The name of your Terraform Cloud organization.
+       organization = "netcool"
+       # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+         name = "common"
+        }
+      }
+    }
+
+#     # An example resource that does nothing.
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+    }
+
+
+
+
+
 provider "random" {
   version = "~> 3.1"
 }
